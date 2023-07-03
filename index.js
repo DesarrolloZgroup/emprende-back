5d4a6d1c878ae231a034ae79a8ea6b78ac9f505b
lpnoobx <lpnoobx@gmail.com>
2023-07-03 14:32:28 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const http = require("http")
+
+function requestController(){
+    //logica
+    console.log("recibimos una nueva request")
+    console.log({global})
+    console.log({dir : __dirname})
+
+}
+//configurar nuestro servidor 
+const server = http.createServer(requestController)
+
+server.listen(4000)
